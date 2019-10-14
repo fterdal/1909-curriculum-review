910e42f60ceb17f05f9242ebf54e3e940342833c
Finn <finnterdal@gmail.com>
2019-10-14 09:19:13 -0500

    router is done

--- app/main.js
+++ app/main.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import {render} from 'react-dom'
+import { render } from 'react-dom'
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom'
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <Router>
+      <div>
+        <nav>
+          <NavLink to="/">Home</NavLink>
+          <br />
+          <NavLink to="/kittens">Kittens</NavLink>
+        </nav>
+        <Switch>
+          <Route path="/kittens" render={() => 'Kittens Page'} />
+          <Route path="/" render={() => 'Homepage'} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
-render(
-  <App />,
-  document.getElementById('main')
-)
+render(<App />, document.getElementById('main'))
