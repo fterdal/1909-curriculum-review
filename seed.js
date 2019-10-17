37843a740e1bd4457443abc399b540dfdb65176f
Finn <finnterdal@gmail.com>
2019-10-17 11:01:31 -0500

    seed many to many

--- seed.js
+++ seed.js
@@ -1,4 +1,4 @@
-const { db, Kitten } = require('./server/db')
+const { db, Kitten, Person } = require('./server/db')
 
 const kittens = [
   {
@@ -21,14 +21,41 @@ const kittens = [
   },
 ]
 
+const people = [
+  { name: 'Bob' },
+  { name: 'Mary' },
+  { name: 'Finn' },
+  { name: 'Priti' },
+  { name: 'Collin' },
+]
+
 const seed = async () => {
   try {
     await db.sync({ force: true })
     console.log('Synced to the database')
-    // const createdKittens = await Kitten.bulkCreate(kittens, { returning: true, validate: true })
-    const createdKittens = await Promise.all(kittens.map(kitten => Kitten.create(kitten)))
-    console.log(createdKittens)
-    console.log(`Seeded ${kittens.length} kittens!`)
+    // const createdKittens = await Promise.all(
+    //   kittens.map(kitten => Kitten.create(kitten))
+    // )
+    const [shelly, gurturde, rigatoni] = await Kitten.bulkCreate(kittens, {
+      returning: true,
+      validate: true,
+    })
+    const [bob, mary, finn] = await Person.bulkCreate(people, {
+      returning: true,
+      validate: true,
+    })
+    // await rigatoni.addPerson(finn)
+    // await gurturde.addPerson(bob)
+    // await shelly.addPerson(mary)
+    // await finn.addKitten(shelly)
+    // await finn.removeKitten(shelly)
+    await Promise.all([
+      rigatoni.addPerson(finn),
+      gurturde.addPerson(bob),
+      shelly.addPerson(mary),
+      finn.addKitten(shelly),
+    ])
+
     db.close()
   } catch (err) {
     console.log(err)
--- server/db/index.js
+++ server/db/index.js
@@ -21,15 +21,24 @@ const Kitten = db.define('kitten', {
       'white',
       'grey',
     ]),
-    // validate: {
-    //   isIn: ['brown', 'orange', 'black', 'calico', 'white', 'grey'],
-    // },
   },
   indoor: Sequelize.BOOLEAN,
   age: Sequelize.FLOAT,
 })
 
+const Person = db.define('person', {
+  name: Sequelize.STRING,
+})
+
+// const JoinTable = db.define('join_table', {
+//   adoptionDate: Sequelize.DATEONLY
+// })
+
+Kitten.belongsToMany(Person, { through: 'adoptions' })
+Person.belongsToMany(Kitten, { through: 'adoptions' })
+
 module.exports = {
   db,
   Kitten,
+  Person,
 }
